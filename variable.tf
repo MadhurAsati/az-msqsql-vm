5e786165a900d7d5cd35c96e544ae99fc6ce08cb
MadhurAsati <madhur9827@gmail.com>
2022-06-15 09:59:46 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "rg_name" {
+  description = "Name of the Resource Group"
+  default = "happy-rg"
+}
+variable "rg_location" {
+ description = "Name of the Region"
+  default = "West Europe"
+}
